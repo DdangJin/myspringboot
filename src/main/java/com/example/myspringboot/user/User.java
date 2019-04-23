27b88ddb77591ae6f29670416c6284bc44e5b0ba
kyle <hdj997@gmail.com>
2019-04-23 15:47:39 +0900

    Swagger 추가 설명 기능 작성

--- src/main/java/com/example/myspringboot/user/User.java
+++ src/main/java/com/example/myspringboot/user/User.java
@@ -1,14 +1,28 @@
 package com.example.myspringboot.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
+@ApiModel(description="All details about the user.")
 public class User {
     private Integer id;
+    @Size(min=2, message="이름은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes="Name should have at least 2 characters")
     private String name;
+
+    @Past
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
+
+    public User() {
+        super();
+    }
 
     public User(Integer id, String name, Date birthDate) {
         this.id = id;
