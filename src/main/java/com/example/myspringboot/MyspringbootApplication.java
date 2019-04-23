7ee17d328962fd4e035ce91ea33b6a9e6f6e30cd
kyle <hdj997@gmail.com>
2019-04-23 11:10:00 +0900

    Change Banner

--- src/main/java/com/example/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/example/myspringboot/MyspringbootApplication.java
@@ -1,7 +1,11 @@
 package com.example.myspringboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class MyspringbootApplication {
@@ -10,6 +14,12 @@ public class MyspringbootApplication {
         //SpringApplication.run(MyspringbootApplication.class, args);
         SpringApplication app = new SpringApplication(
                                         MyspringbootApplication.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.print("\n\nTest My Banner!\n\n".toUpperCase());
+            }
+        });
 
         app.run(args);
     }
