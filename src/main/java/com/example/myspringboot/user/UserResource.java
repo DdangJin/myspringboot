4cb68019e80c181fc980a089b7c6920e541d913c
kyle <hdj997@gmail.com>
2019-04-23 14:49:54 +0900

    REST level 3 HATEOAS users/{id} 구현 단, 브라우저에선 작동안함, Postman으로 볼 것

--- src/main/java/com/example/myspringboot/user/UserResource.java
+++ src/main/java/com/example/myspringboot/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.example.myspringboot.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -19,10 +24,22 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public Resource<User> retrieveUser(@PathVariable int id) {
         User user =  service.findOne(id);
 
-        return user;
+        if (user == null)
+            throw new UserNotFoundException("id-" + id);
+
+        // "all-users", SERVER_PATH + "/users"
+        // retrieveAllUsers();
+        // HATEOAS
+        Resource<User> resource = new Resource<User>(user);
+
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
     }
 
     @PostMapping("/users")
