5807fde431f2cd3976adc97bc371b3c16de004e0
kyle <hdj997@gmail.com>
2019-04-23 16:21:47 +0900

    Users에 valid 과정 추가, 모니터링툴인 Actuator와 HAL Browser 적용

--- src/main/java/com/example/myspringboot/user/UserResource.java
+++ src/main/java/com/example/myspringboot/user/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -43,7 +44,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         // /users/{id}
