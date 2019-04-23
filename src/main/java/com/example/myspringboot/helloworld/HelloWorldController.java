740186691d42ae350e7c7d92ea0f34b9de189d8d
kyle <hdj997@gmail.com>
2019-04-23 15:24:12 +0900

    Internationalized 구현

--- src/main/java/com/example/myspringboot/helloworld/HelloWorldController.java
+++ src/main/java/com/example/myspringboot/helloworld/HelloWorldController.java
@@ -1,11 +1,24 @@
 package com.example.myspringboot.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage(
+                "good.afternoon.message", null,
+                LocaleContextHolder.getLocale());
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
