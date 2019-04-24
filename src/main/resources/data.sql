a3f790c2fafd7f3dfb75ab03406106ce56d4cd3c
kyle <hdj997@gmail.com>
2019-04-24 13:39:29 +0900

    data.sql user, post insert query 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,10 @@ insert into account(id, account_name, password, enabled1, enabled2, enabled3, en
 
 insert into account(id, account_name, password, enabled1, enabled2, enabled3, enabled4)
     values(2, 'user1', 'test1234', true, true, true, true);
+
+insert into user values(10001, sysdate(), 'AB');
+insert into user values(10002, sysdate(), 'Jill');
+insert into user values(10003, sysdate(), 'Jam');
+
+insert into post values(11001, 'My First Post', 10001);
+insert into post values(11002, 'My Second Post', 10001);
